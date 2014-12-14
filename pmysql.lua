7016739ab580e415c6e5676b54125a4b835ab43f
aStonedPenguin <zachary000@gmail.com>
2014-12-14 11:13:17 -0900

    Update pmysql.lua

--- pmysql.lua
+++ pmysql.lua
@@ -2,40 +2,76 @@ require( 'tmysql4' );
 
 pmysql = { };
 
-local db_cache = {}
+local db_cache = { };
 
 local db_mt = { };
 db_mt.__index = db_mt;
 
 local function print( ... )
-  return MsgC(Color(0,255,0), '[MYSQL] ', Color(255,255,255), ... .. '\n')
+  return MsgC( Color( 0, 255, 0 ), '[MYSQL] ', Color( 255, 255, 255 ), ... .. '\n' )
 end
 
 function pmysql.newdb( HOSTNAME, USERNAME, PASSWORD, DATABASE, PORT, OPTIONAL_UNIX_SOCKET_PATH )
   local obj = { };
   setmetatable( obj, db_mt );
   
-  obj.hash = string.format( '%s:%s@%X:%s', HOSTNAME, PORT, util.CRC( USERNAME .. '-' .. PASSWORD ), DATABASE );
+  obj.hash            = string.format( '%s:%s@%X:%s', HOSTNAME, PORT, util.CRC( USERNAME .. '-' .. PASSWORD ), DATABASE );
 
   if db_cache[ obj.hash ] then
-    obj.db = db_cache[ obj.hash ];
-    return print( 'Recycled database connection with hashid: ' .. obj.hash );
-  end
-
-  obj.db = tmysql.initialize( HOSTNAME, USERNAME, PASSWORD, DATABASE, PORT, OPTIONAL_UNIX_SOCKET_PATH );
-  
-  if obj.db then 
-    print( 'Connected to database ' .. DATABASE .. ':' .. PORT .. ' successfully.' );
-  else
-    print( 'Connection to database ' .. DATABASE .. ':' .. PORT .. ' failed.' );
+    obj._db = db_cache[ obj.hash ]._db;
+    print( 'Recycled database connection with hashid: ' .. obj.hash );
     return
   end
 
+  obj._db, obj.err   = tmysql.initialize( HOSTNAME, USERNAME, PASSWORD, DATABASE, PORT, OPTIONAL_UNIX_SOCKET_PATH );
+  obj.hostname       = HOSTNAME;
+  obj.username       = USERNAME;
+  obj.password       = PASSWORD;
+  obj.database       = DATABASE;
+  obj.port           = PORT;
+
+  if obj._db then 
+    print( 'Connected to database ' .. DATABASE .. ':' .. PORT .. ' successfully.' );
+  elseif obj.err then
+    print( 'Connection to database ' .. DATABASE .. ':' .. PORT .. ' failed. ERROR: ' .. obj.err );
+    return
+  end
+
+  db_cache[ obj.hash ] = obj;
+
   return obj;
+end
+pmysql.connect = pmysql.newdb;
+
+function pmysql.getTable( )
+  return db_cache;
+end
+
+function pmysql.pollAll( )
+  for _, db in pairs( pmysql.getTable() ) do
+    db._db:poll( );
+  end
+end
+
+function db_mt:escape( str )
+  return tmysql.escape( tostring( str ) );
+end
+
+function db_mt:poll( )
+  return self._db:Poll( );
+end
+
+function db_mt:setCharset( charset )
+  return self._db:SetCharset( charset );
+end
+
+function db_mt:disconnect( )
+  self._db:Disconnect( );
+  db_cache[ self ] = nil;
 end
 
 function db_mt:query( sqlstr, cback )
-  return self.db:Query( sqlstr, function( data, status, err )
+  return self._db:Query( sqlstr, function( data, status, err )
     if status then
       if cback then cback( data ); end
     elseif err then
@@ -45,12 +81,14 @@ function db_mt:query( sqlstr, cback )
 end
 
 function db_mt:query_ex( sqlstr, options, cback )
-  table.foreach( options, function( k, v )
-    options[ k ] = self:escape( v );
-  end );
+  if options ~= nil then
+    table.foreach( options, function( k, v )
+      options[ k ] = self:escape( v );
+    end );
 
-  sqlstr = sqlstr:gsub( '%%','%%%%' ):gsub( '?', '%%s' );
-  sqlstr = string.format( sqlstr, unpack( options ) );
+    sqlstr = sqlstr:gsub( '%%','%%%%' ):gsub( '?', '%%s' );
+    sqlstr = string.format( sqlstr, unpack( options ) );
+  end
   return self:query( sqlstr, cback );
 end
 
@@ -63,12 +101,8 @@ function db_mt:query_sync( sqlstr, options )
   end );
 
   while not done do
-    self.db:Poll( );
+    self:poll( );
   end
 
   return data;
-end
-
-function db_mt:escape( str )
-  return tmysql.escape( tostring( str ) );
 end
